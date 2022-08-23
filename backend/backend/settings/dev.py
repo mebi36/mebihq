17f1fc19b11372f22343f93cdb1ca0ee69194b77
mebi36 <chukwudumebinwobi@gmail.com>
2022-08-23 09:17:02 +0100

    changed settings for deployment

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -1,13 +1,14 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-@jyt$g=b*8$dypay&zbpff#y*vrum9fv=#xyyuwybd*9grggk^"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["mebihq.herokuapp.com", "localhost"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
