ed7a063c6119cdc7dcf71808155994c927cb6ee5
mebi36 <chukwudumebinwobi@gmail.com>
2022-08-23 14:13:04 +0100

    removed axios base url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios'
+// import axios from 'axios'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,7 +8,8 @@ import '@marcoschulte/vue3-progress/dist/index'
 import { Vue3ProgressPlugin } from '@marcoschulte/vue3-progress'
 import 'bootstrap'
 
-axios.defaults.baseURL = 'http://localhost:8009'
+// axios.defaults.baseURL = 'http://localhost:8009'
+// axios.defaults.baseURL = 'http://mebihq.herokuapp.com'
 createApp(App)
 .use(Vue3ProgressPlugin)
 .use(router)
