64f8b88f1f3b634808a44ec12ca1c2626144fc14
mebi36 <chukwudumebinwobi@gmail.com>
2022-08-23 10:50:59 +0100

    fixing static file issue

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -156,7 +156,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static"),
+    # os.path.join(PROJECT_DIR, "static"),
     os.path.join(BASE_DIR, "static_files"),
 
 ]
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/4.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -1,8 +1,8 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-@jyt$g=b*8$dypay&zbpff#y*vrum9fv=#xyyuwybd*9grggk^"
