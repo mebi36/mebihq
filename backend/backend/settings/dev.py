526ffdfeceb08ecb7176aa6670e54dca92d018d8
mebi36 <chukwudumebinwobi@gmail.com>
2025-06-03 06:53:52 +0100

    added ec2 ip to allowed hosts

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -8,7 +8,7 @@ DEBUG = True
 SECRET_KEY = "django-insecure-@jyt$g=b*8$dypay&zbpff#y*vrum9fv=#xyyuwybd*9grggk^"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["mebihq.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["mebihq.herokuapp.com", "localhost", "35.172.194.18"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
